0edebddcfd99b63484fed92fe58c1b8fef65770b
Jamie DeLong <jdlong1980@gmail.com>
2020-08-04 00:56:25 -0400

    final touches to admin page

--- api-routes.js
+++ api-routes.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
 var db = require("./models");
 
+function verifyUser(req, res, next) {
+  const username = req.query.username || req.body.username;
+  const password = req.query.password || req.body.password;
+  if (username === process.env.USERNAME && password === process.env.PASSWORD) return next();
+  return res.status(401).end();
+};
+
 module.exports = function (app) {
+  app.post("/api/login", verifyUser, function (req, res) {
+    res.status(200).send("Logged In");
+  });
+
   app.get("/api/server", function (req, res) {
     res.status(200).send("Server Online");
   });
 
-  app.get("/api/responses", function (req, res) {
+  app.get("/api/responses", verifyUser, function (req, res) {
     db.Questionnaire.findAll({ active: true })
       .then(data => {
         res.status(200).json(data);
